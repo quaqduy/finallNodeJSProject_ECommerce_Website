fd8ec46741b901097cd4f4bbf7beac1d80f9df07
Quang Duy <87593754+quaqduy@users.noreply.github.com>
2024-11-27 00:32:04 +0700

    handle db config

--- config/db.js
+++ config/db.js
@@ -5,13 +5,13 @@ require('dotenv').config();
 mongoose.set('strictQuery', true);
 
 // Construct the connection string dynamically
-const atlat = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-console.log(atlat); // Check the constructed connection string
+console.log(connectionString); // Check the constructed connection string
 
 const connect = async () => {
     try {
-        await mongoose.connect(atlat);
+        await mongoose.connect(connectionString);
         console.log("Connect success");
     } catch (err) {
         console.log("Connect fail");
