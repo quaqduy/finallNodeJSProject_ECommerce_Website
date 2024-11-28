cde93240061fad0dd239868934248162be7fd47f
Anh Khoa <521h0459@student.tdtu.edu.vn>
2024-11-28 15:04:24 +0700

    Login + Shop + Product-details UI

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,48 @@ router.get('/checkout', function(req, res, next) {
   res.render('checkout', { title: 'Express' });
 });
 
+router.get('/shop', function(req, res, next) {
+  res.render('shop', { title: 'Express' });
+});
+
+router.get('/product-details', function(req, res, next) {
+  res.render('product-details', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+router.get('/my-account', function(req, res, next) {
+  res.render('my-account', { title: 'Express' });
+});
+
+router.get('/shop', function(req, res, next) {
+  res.render('shop', { title: 'Express' });
+});
+
+router.get('/wishlist', function(req, res, next) {
+  res.render('wishlist', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
+router.get('/blog', function(req, res, next) {
+  res.render('blog', { title: 'Express' });
+});
+
+router.get('/blog-details', function(req, res, next) {
+  res.render('blog-details', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+router.get('/faq', function(req, res, next) {
+  res.render('faq', { title: 'Express' });
+});
+
 module.exports = router;
