af214f96c2e7acad3d4be15204b8e01d3261a542
Quang Duy <87593754+quaqduy@users.noreply.github.com>
2024-12-03 15:19:13 +0700

    complete detail-item view

--- public/javascripts/productDetail.js
+++ public/javascripts/productDetail.js
@@ -14,3 +14,33 @@ const detailHandle = {
 
 detailHandle.start();
 
+
+document.querySelector('#quantity').addEventListener('input',(e)=>{
+    const stockMessageClass = 'stock-message'; 
+    const existingMsg = document.querySelector(`.${stockMessageClass}`);    
+
+    const stock = document.querySelector('#stockProduct').value;
+    
+    document.querySelector('#btnSubmitCartItem').disabled = false;
+
+    if(parseInt(stock) < parseInt(e.target.value)){
+        if(!existingMsg){
+            const msg = document.createElement('div');
+            msg.innerText = 'The quantity you chose is out of the amount in stock. There are '+this.current_productQuantity+' available.';
+            msg.style.color = 'red';
+            msg.style.fontWeight = 'bold';
+            msg.classList.add(stockMessageClass);
+            document.querySelector('#msgProductDetail').appendChild(msg);
+        }
+        document.querySelector('#btnSubmitCartItem').disabled = true;
+    }
+    else if (existingMsg) { 
+        existingMsg.remove(); 
+    }
+})
+
+const addToWishlist = ()=>{
+    document.querySelector('.wishlistAdd_form input[name = "currentUrl"]').value = window.location.href;
+
+    document.querySelector('.wishlistAdd_form input[type = "submit"]').click();
+}
