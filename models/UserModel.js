611480dd42300b21052d03cd42b8b77542491920
DungRuoii <ballackno3@gmail.com>
2024-12-09 02:23:20 +0700

    update admin function

--- models/UserModel.js
+++ models/UserModel.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true }, // Lưu mật khẩu đã mã hóa
     email: { type: String, required: true },
     phoneNumber: { type: String, required: true },
-    role: { type: String, enum: ['customer', 'admin'], default: 'customer' },
+    role: { type: String, enum: ['customer', 'banned', 'admin'], default: 'customer' },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
 });
